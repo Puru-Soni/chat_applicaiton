49b1dfa2d037753d0177b14137f1de22a9e40dac
Puru-Soni <puru20010105@gmail.com>
2022-10-26 23:47:48 +0530

    initalizing the app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss';
+import './App.css';
+
+function App() {
+  return <div className="App">My Chat App</div>;
+}
+
+export default App;
